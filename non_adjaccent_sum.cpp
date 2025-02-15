44e7da99b7207a9116819d05e642e9eb8e32a57a
pvy-it-tdh <phucvy107@gmail.com>
2025-02-15 10:07:30 +0700

    add file

--- non_adjaccent_sum.cpp
+++ non_adjaccent_sum.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int main()
 {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+    vector<int> dp(n);
+    for (int i = 1; i <= n; i++)
+    {
+        dp[i] = max(dp[i - 1], dp[i - 2] + a[i]);
+    }
+    cout << *max_element(dp.begin(), dp.end());
 }
